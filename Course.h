8f08a8bbb5c67ec444374bb9e086444403ee2a17
Mohamed Essam Elsadany <140955930+MElsadany2165@users.noreply.github.com>
2025-11-11 09:32:51 +0200

    Implement Instructor class in Course.h
    Added Instructor class to manage course details and students.

--- Course.h
+++ Course.h
@@ -1,22 +1,30 @@
 #ifndef COURSE_H
 #define COURSE_H
-
+#include <iostream>
 #include <string>
 #include "Student.h"
 using namespace std;
 //#write your code here
 
 
+class Instructor : public Student {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+
+public:
+void addStudent(const Student& s);
+void displayCourseInfo(){
+    cout << "Course: " << courseCode << "-" << courseName << endl;;
+    cout << "Max Students: " << maxStudents << endl;
+    cout << "Current Enrolled: " << currentStudents << endl;
+}
 
 
-
-
-
-
-
-
-
-
+};
 
 
 #endif
