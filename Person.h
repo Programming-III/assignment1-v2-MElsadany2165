03e5d20465a57a33429627a2e97a5e879ae75787
Mohamed Essam Elsadany <140955930+MElsadany2165@users.noreply.github.com>
2025-11-11 09:31:42 +0200

    Implement Person class with display method
    Added Person class with name and id attributes and a display method.

--- Person.h
+++ Person.h
@@ -1,16 +1,22 @@
 #ifndef PERSON_H
 #define PERSON_H
-
+#include <iostream>
 #include <string>
 using namespace std;
 
 //#write Person class here
+class Person{
+private:
+    string name;
+    int id;
 
+public:
+string display(){
 
-
-
-
-
+    cout << "Person's Name: "<< name << endl;
+    cout << "Person's ID: " << id << endl;
+}
+};
 
 
 
